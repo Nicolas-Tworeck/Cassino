dc673ffbbba696bb990a91596bcb6828de9ba7d9
Nicolas-Tworeck <nico.tworeck@gmail.com>
2024-11-07 20:04:33 -0300

    commit

--- script/script.js
+++ script/script.js
@@ -16,3 +16,10 @@ const mascaraTelefone = (valor) => {
     valor = valor.replace(/(\d)(\d{4})$/, "$1-$2")
     tel.value = valor
 }
+
+//CPF
+var cpf = document.querySelector("#cpf");
+
+cpf.addEventListener("blur", function(){
+   if(cpf.value) cpf.value = cpf.value.match(/.{1,3}/g).join(".").replace(/\.(?=[^.]*$)/,"-");
+});
