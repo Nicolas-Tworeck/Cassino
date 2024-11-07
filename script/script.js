9df274fc6b6ecfce2ecf58cbc82ebbef8e21f2c1
Nicolas-Tworeck <nico.tworeck@gmail.com>
2024-11-06 23:38:01 -0300

    commit

--- script/script.js
+++ script/script.js
@@ -4,3 +4,15 @@ function cadastro(){
 function login(){
     window.location.href = "index.html";
 }
+
+//telefone
+const tel = document.getElementById('tel') 
+
+tel.addEventListener('keypress', (e) => mascaraTelefone(e.target.value)) 
+
+const mascaraTelefone = (valor) => {
+    valor = valor.replace(/\D/g, "")
+    valor = valor.replace(/^(\d{2})(\d)/g, "($1) $2")
+    valor = valor.replace(/(\d)(\d{4})$/, "$1-$2")
+    tel.value = valor
+}
