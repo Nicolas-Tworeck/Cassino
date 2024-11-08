a9f2f947c09e697893eb5dbb61d84018d801a2fc
Nicolas-Tworeck <nico.tworeck@gmail.com>
2024-11-07 22:00:45 -0300

    commit

--- script/script.js
+++ script/script.js
@@ -23,3 +23,21 @@ var cpf = document.querySelector("#cpf");
 cpf.addEventListener("blur", function(){
    if(cpf.value) cpf.value = cpf.value.match(/.{1,3}/g).join(".").replace(/\.(?=[^.]*$)/,"-");
 });
+
+//senhas
+let senha = document.getElementById('senha');
+let senhaC = document.getElementById('senhaC');
+
+function validarSenha() {
+  if (senha.value != senhaC.value) {
+    senhaC.setCustomValidity("Senhas diferentes!");
+    senhaC.reportValidity();
+    return false;
+  } else {
+    senhaC.setCustomValidity("");
+    return true;
+  }
+}
+
+// verificar também quando o campo for modificado, para que a mensagem suma quando as senhas forem iguais
+senhaC.addEventListener('input', validarSenha);
