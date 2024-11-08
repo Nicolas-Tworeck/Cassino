51a12832d7982937249d4e3de730ec27a83add6b
Nicolas-Tworeck <nico.tworeck@gmail.com>
2024-11-08 16:46:05 -0300

    commit

--- script/script.js
+++ script/script.js
@@ -4,6 +4,9 @@ function cadastro(){
 function login(){
     window.location.href = "index.html";
 }
+function contrato(){
+  window.location.href = "contrato.html";
+}
 
 //telefone
 const tel = document.getElementById('tel') 
@@ -18,11 +21,20 @@ const mascaraTelefone = (valor) => {
 }
 
 //CPF
-var cpf = document.querySelector("#cpf");
+function mascara(i){
+   
+  var v = i.value;
+  
+  if(isNaN(v[v.length-1])){ // impede entrar outro caractere que não seja número
+     i.value = v.substring(0, v.length-1);
+     return;
+  }
+  
+  i.setAttribute("maxlength", "14");
+  if (v.length == 3 || v.length == 7) i.value += ".";
+  if (v.length == 11) i.value += "-";
 
-cpf.addEventListener("blur", function(){
-   if(cpf.value) cpf.value = cpf.value.match(/.{1,3}/g).join(".").replace(/\.(?=[^.]*$)/,"-");
-});
+}
 
 //senhas
 let senha = document.getElementById('senha');
