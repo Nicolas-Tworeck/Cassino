2f179d6d87d48ab6af88d7a9501f536d16fa4ae1
Nicolas-Tworeck <nico.tworeck@gmail.com>
2024-11-12 18:53:35 -0300

    commit

--- script/script.js
+++ script/script.js
@@ -1,31 +1,34 @@
-function cadastro(){
-    window.location.href = "cadastro.html";
+function cadastro() {
+  window.location.href = "cadastro.html";
 }
-function login(){
-    window.location.href = "index.html";
+function login() {
+  window.location.href = "index.html";
 }
-function contrato(){
+function contrato() {
   window.location.href = "contrato.html";
 }
 
+
+
+
 //telefone
-$(document).ready(function() {
+$(document).ready(function () {
   $("#telefone").inputmask({
-    mask: ["(99) 9999-9999", "(99) 99999-9999", ],
+    mask: ["(99) 9999-9999", "(99) 99999-9999",],
     keepStatic: true
   });
 })
 
 //CPF
-function mascara(i){
-   
+function mascara(i) {
+
   var v = i.value;
-  
-  if(isNaN(v[v.length-1])){ // impede entrar outro caractere que não seja número
-     i.value = v.substring(0, v.length-1);
-     return;
+
+  if (isNaN(v[v.length - 1])) {
+    i.value = v.substring(0, v.length - 1);
+    return;
   }
-  
+
   i.setAttribute("maxlength", "14");
   if (v.length == 3 || v.length == 7) i.value += ".";
   if (v.length == 11) i.value += "-";
@@ -47,5 +50,4 @@ function validarSenha() {
   }
 }
 
-// verificar também quando o campo for modificado, para que a mensagem suma quando as senhas forem iguais
 senhaC.addEventListener('input', validarSenha);
