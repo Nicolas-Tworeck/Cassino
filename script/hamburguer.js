657445b6f9dce823245b10448b4a8d22b37459f6
Nicolas-Tworeck <nico.tworeck@gmail.com>
2024-11-29 17:14:24 -0300

    commit

---
+++ script/hamburguer.js
@@ -0,0 +1,26 @@
+//hamburguer
+
+document.addEventListener("DOMContentLoaded", () => {
+    const menuButton = document.getElementById("menuButton");
+    const sideMenu = document.getElementById("sideMenu");
+    const closeButton = document.getElementById("closeButton");
+    const body = document.body;
+  
+    menuButton.addEventListener("click", () => {
+        sideMenu.classList.add("open");
+        body.style.overflow = "hidden"; // Impede rolagem
+    });
+  
+    closeButton.addEventListener("click", () => {
+        sideMenu.classList.remove("open");
+        body.style.overflow = ""; // Restaura rolagem
+    });
+  
+    // Fecha o menu ao clicar fora dele
+    window.addEventListener("click", (e) => {
+        if (!sideMenu.contains(e.target) && e.target !== menuButton) {
+            sideMenu.classList.remove("open");
+            body.style.overflow = ""; // Restaura rolagem
+        }
+    });
+  });
--- script/script.js
+++ script/script.js
@@ -51,3 +51,4 @@ function validarSenha() {
 }
 
 senhaC.addEventListener('input', validarSenha);
+
