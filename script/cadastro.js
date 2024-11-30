409cb13ddb435dbe3490a9dc277a0d335dcdd0c7
Nicolas-Tworeck <nico.tworeck@gmail.com>
2024-11-30 00:15:15 -0300

    commit

--- script/cadastro.js
+++ script/cadastro.js
@@ -255,3 +255,45 @@ function mascaraCEP(cep) {
 document.getElementById('cep').addEventListener('input', function() {
   this.value = mascaraCEP(this.value);
 });
+
+//telefone
+$(document).ready(function () {
+  $("#telefone").inputmask({
+    mask: ["(99) 9999-9999", "(99) 99999-9999",],
+    keepStatic: true
+  });
+})
+
+//CPF
+function mascara(i) {
+
+  var v = i.value;
+
+  if (isNaN(v[v.length - 1])) {
+    i.value = v.substring(0, v.length - 1);
+    return;
+  }
+
+  i.setAttribute("maxlength", "14");
+  if (v.length == 3 || v.length == 7) i.value += ".";
+  if (v.length == 11) i.value += "-";
+
+}
+
+//senhas
+let senha = document.getElementById('senha');
+let senhaC = document.getElementById('senhaC');
+
+function validarSenha() {
+  if (senha.value != senhaC.value) {
+    senhaC.setCustomValidity("Senhas diferentes!");
+    senhaC.reportValidity();
+    return false;
+  } else {
+    senhaC.setCustomValidity("");
+    return true;
+  }
+}
+
+senhaC.addEventListener('input', validarSenha);
+
--- script/script.js
+++ script/script.js
@@ -10,48 +10,13 @@ function contrato() {
 function contato(){
   window.location.href = "https://www.instagram.com/vegass.bet/profilecard/?igsh=em0zN2E0eTgxbnNu";
 }
-
-
-
-
-//telefone
-$(document).ready(function () {
-  $("#telefone").inputmask({
-    mask: ["(99) 9999-9999", "(99) 99999-9999",],
-    keepStatic: true
-  });
-})
-
-//CPF
-function mascara(i) {
-
-  var v = i.value;
-
-  if (isNaN(v[v.length - 1])) {
-    i.value = v.substring(0, v.length - 1);
-    return;
-  }
-
-  i.setAttribute("maxlength", "14");
-  if (v.length == 3 || v.length == 7) i.value += ".";
-  if (v.length == 11) i.value += "-";
-
+function profile(){
+  window.location.href = "profile.html";
+}
+function index(){
+  window.location.href = "index.html";
 }
 
-//senhas
-let senha = document.getElementById('senha');
-let senhaC = document.getElementById('senhaC');
 
-function validarSenha() {
-  if (senha.value != senhaC.value) {
-    senhaC.setCustomValidity("Senhas diferentes!");
-    senhaC.reportValidity();
-    return false;
-  } else {
-    senhaC.setCustomValidity("");
-    return true;
-  }
-}
 
-senhaC.addEventListener('input', validarSenha);
 
